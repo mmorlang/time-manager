211420613749028c3846196dc1307779c8bf555d
Mickie Morlang <mmorlang@gmail.com>
2020-07-03 19:23:05 -0600

    added time manager application.

--- app/src/main/java/edu/cnm/deepdive/timemanager/TimeManagerApplication.java
+++ app/src/main/java/edu/cnm/deepdive/timemanager/TimeManagerApplication.java
@@ -1,7 +1,21 @@
 package edu.cnm.deepdive.timemanager;
 
 import android.app.Application;
+import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.timemanager.service.TimeManagerDatabase;
+import io.reactivex.schedulers.Schedulers;
 
 public class TimeManagerApplication extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    TimeManagerDatabase.setContext(this);
+    TimeManagerDatabase database = TimeManagerDatabase.getInstance();
+    database.getTimeframeDao().delete()
+        .subscribeOn(Schedulers.io())
+        .subscribe();
+    Stetho.initializeWithDefaults(this);
+  }
 
 }
+
